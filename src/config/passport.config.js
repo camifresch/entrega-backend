8f9be9b1488e250789e66935e4d083a8abcce6ac
Cami Fresch <cami.fresch@hotmail.com>
2023-08-29 02:58:03 -0300

    req

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -6,45 +6,84 @@ import { createHash } from "../utils.js";
 
 const LocalStrategy = local.Strategy;
 
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-
 const initializePassport = async () => {
   const githubData = {
     clientID: 'Iv1.574f670264322cb9',
-    clientSecret: '3d88a97a3e5f96c4c059b0c78b313232783a99ce',
-    callbackUrl: 'http://localhost:3000/api/sessions/githubcallback'
+        clientSecret: '3d88a97a3e5f96c4c059b0c78b313232783a99ce',
+        callbackUrl: 'http://localhost:3000/api/sessions/githubcallback'
+        
+    /* clientID: "Iv1.2160438db8b06c53",
+    clientSecret: "88dfabd35a94c47466d448cae5d1d0274711355b",
+    callbackUrl: "http://localhost:3000/api/sessions/githubcallback", */
   };
 
-  passport.use(new GitHubStrategy({
-    clientID: githubData.clientID,
-    clientSecret: githubData.clientSecret,
-    callbackURL: githubData.callbackUrl
-  }, (accessToken, refreshToken, profile, done) => {
-    
+  const verifyAuthGithub = async (accessToken, refreshToken, profile, done) => {
     try {
       console.log(profile);
-      const user = userModel.findOne({ userName: profile._json.email });
+      const user = await userModel.findOne({ userName: profile._json.email });
 
       if (!user) {
-        done(null, false);
+        req.sessionStore.userValidated = true;
+        return done(null, profile);
+
       } else {
-        done(null, user);
+        req.sessionStore.userValidated = false;
+
+        return done(null, false);
+  
       }
     } catch (err) {
       return done(err.message);
     }
-    return done(null, profile);
-  }));
+  };
 
+  passport.use("github", new GithubStrategy(githubData, verifyAuthGithub));
+
+  passport.use(
+    "register",
+    new LocalStrategy(
+      {
+        passReqToCallback: true,
+        usernameField: "userName",
+        passwordField: "password",
+        session: false,
+      },
+      async (userName, password, done) => {
+        try {
+          const exists = await userService.getUserBy({ userName: userName });
+          if (exists)
+            return done(null, false, {
+              message: "El usuario ya se encuentra registrado",
+            });
+
+          const newUser = {
+            firstName: firstName,
+            lastName: lastName,
+            userName: userName,
+            password: createHash(password),
+          };
+
+          let result = await userService.addUser(newUser);
+          return done(null, result);
+        } catch (err) {
+          return done(err.message);
+        }
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user._id);
   });
 
-  passport.deserializeUser((user, done) => {
-    done(null, user);
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await userModel.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err.message);
+    }
   });
 };
 
-module.exports = initializePassport;
+export default initializePassport;
